fd08e5a7ef8c1f5010c0b0e45959cd1d631aa053
SuvamNaskar <suvamnaskar780@gmail.com>
2023-11-08 12:25:40 +0530

    Create CourseapidataApplicationTests.java

---
+++ src/test/java/in/suvam/courseapidata/CourseapidataApplicationTests.java
@@ -0,0 +1,13 @@
+package in.suvam.courseapidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseapidataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
